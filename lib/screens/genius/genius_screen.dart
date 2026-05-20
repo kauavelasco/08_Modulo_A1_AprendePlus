f6452d9b84bb58d51949f0f867704dbb5374b363
Kauã Velasco <kauavelasco1205@gmail.com>
2026-05-20 16:29:57 -0300

    config(genius): start layout and functions

--- lib/screens/genius/genius_screen.dart
+++ lib/screens/genius/genius_screen.dart
@@ -11,6 +11,42 @@ class _GeniusScreenState extends State<GeniusScreen> {
   String itemSelecionado = "Português";
   final List<String> options = ['Português', 'English', 'Español'];
 
+  bool emJogo = false;
+  int score = 0;
+  int nivel = 1;
+  
+
+  void adicionarPonto(int ponto) {
+    setState(() {
+      score += ponto;
+    });
+  }
+
+  void proximoNivel() {
+    setState(() {
+      nivel++;
+    });
+  }
+
+  void resetGame() async {
+    setState(() {
+      emJogo = false;
+      nivel = 1;
+      score = 0;
+    });
+    await Future.delayed(Duration(seconds: 2));
+    setState(() {
+      emJogo = true;
+    });
+  }
+
+  void finalizarJogo() {
+    setState(() {
+      emJogo = false;
+      score = 0;
+    });
+  }
+
   Widget colorButton({
     required Color cor,
     required VoidCallback onTap,
@@ -44,41 +80,44 @@ class _GeniusScreenState extends State<GeniusScreen> {
         actions: [
           SizedBox(
             width: 130, 
-            child: DropdownButtonFormField<String>(
-              initialValue: itemSelecionado,
-              dropdownColor: Colors.white,
-              decoration: InputDecoration(
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xFF3C00A7)),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xFF3C00A7)),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xFF3C00A7), width: 2),
-                ),
-                contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              ),
-              items: options.map((String opcao) {
-                return DropdownMenuItem<String>(
-                  value: opcao,
-                  child: Text(
-                    opcao,
-                    style: TextStyle(color: Color(0xFF3C00A7)),
+            child: IgnorePointer(
+              ignoring: emJogo ? true : false,
+              child: DropdownButtonFormField<String>(
+                initialValue: itemSelecionado,
+                dropdownColor: Colors.white,
+                decoration: InputDecoration(
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF3C00A7)),
                   ),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  itemSelecionado = value!;
-                });
-              },
-              validator: (value) {
-                if (value == null) {
-                  return 'Selecione uma opção';
-                }
-                return null;
-              },
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF3C00A7)),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF3C00A7), width: 2),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                ),
+                items: options.map((String opcao) {
+                  return DropdownMenuItem<String>(
+                    value: opcao,
+                    child: Text(
+                      opcao,
+                      style: TextStyle(color: Color(0xFF3C00A7)),
+                    ),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    itemSelecionado = value!;
+                  });
+                },
+                validator: (value) {
+                  if (value == null) {
+                    return 'Selecione uma opção';
+                  }
+                  return null;
+                },
+              ),
             ),
           ),
           SizedBox(width: 16),
@@ -92,7 +131,7 @@ class _GeniusScreenState extends State<GeniusScreen> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  'Nível: 1',
+                  'Nível: $nivel',
                   style: TextStyle(
                     color: Color(0xFF3C00A7),
                     fontSize: 18,
@@ -134,9 +173,18 @@ class _GeniusScreenState extends State<GeniusScreen> {
                         borderRadius: BorderRadius.circular(16),
                       ),
                     ),
-                    onPressed: () {}, 
+                    onPressed: emJogo ? () {
+                      finalizarJogo();
+                      setState(() {
+                        emJogo = false;
+                      });
+                    } : () {
+                      setState(() {
+                        emJogo = true;
+                      });
+                    }, 
                     child: Text(
-                      'Start',
+                      emJogo ? 'Stop' : 'Start',
                       style: TextStyle(
                         color: Color(0xFFFCFCFC),
                         fontSize: 18,
@@ -179,7 +227,7 @@ class _GeniusScreenState extends State<GeniusScreen> {
                 ),
                 SizedBox(width: 20),
                 Text(
-                  '0000',
+                  score.toString().padLeft(4, '0'),
                   style: TextStyle(
                     color: Color(0xFF3C00A7),
                     fontSize: 18,
@@ -194,7 +242,7 @@ class _GeniusScreenState extends State<GeniusScreen> {
                       borderRadius: BorderRadius.circular(16),
                     ),
                   ),
-                  onPressed: () {},
+                  onPressed: emJogo ? resetGame : null,
                   child: Text(
                     'Restart',
                     style: TextStyle(
